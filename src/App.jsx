72a9847e93a8608f6305d69d8028085603a2dcfe
Philemon Abakah <philemon.abakah@hubtel.com>
2025-08-11 00:35:14 +0000

    feat: developed header , her o , features and pricing section

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Features from './sections/Features'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Pricing from './sections/Pricing'
+
+function App() {
+
+  return (
+    <main className='overflow-hidden'>
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+    </main>
+  )
+}
+
+export default App
